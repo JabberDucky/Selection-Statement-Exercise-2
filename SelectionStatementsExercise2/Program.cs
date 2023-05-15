9799444eb82ba54441ea2a7920402c6c74cb3d85
Katherine Vail <jabberduckygame@gmail.com>
2023-05-15 15:00:05 -0500

    Completed exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Subject();
+        }
+        public static void Subject()
+        {
+            Console.WriteLine("What Is your favorite subject?");
+            var answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "math":
+                    Console.WriteLine("Math makes my brain go fuzzy.");
+                    break;
+                case "art":
+                    Console.WriteLine("The best subject of them all.");
+                    break;
+                case "science":
+                    Console.WriteLine("Pluto will always be a planet to me.");
+                    break;
+                case "history":
+                    Console.WriteLine("Learning it so we don't repeat it.");
+                    break;
+                case "english":
+                    Console.WriteLine("They're, their, and there. Make sure you're using the right one.");
+                    break;
+                default:
+                    Console.WriteLine("Please choose a different subject!");
+                    break;
+            }
         }
     }
 }
